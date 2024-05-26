9c3eb6ab71a60c75e6d005c1cc76ad6f7af0ad63
GraftingRayman <156515434+GraftingRayman@users.noreply.github.com>
2024-05-26 10:07:57 +0100

    new nodes

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
-from .nodes import GRPromptSelector, GRImageResize, GRMaskResize, GRMaskCreate, GRMultiMaskCreate, GRImageSize, GRTileImage
-NODE_CLASS_MAPPINGS = { "GR Prompt Selector" : GRPromptSelector , "GR Image Resize" : GRImageResize, "GR Mask Resize" : GRMaskResize, "GR Mask Create" : GRMaskCreate, "GR Multi Mask Create" : GRMultiMaskCreate, "GR Image Size": GRImageSize, "GR Tile and Border Image": GRTileImage }
+from .nodes import GRPromptSelector, GRImageResize, GRMaskResize, GRMaskCreate, GRMultiMaskCreate, GRImageSize, GRTileImage, GRPromptSelectorMulti, GRTileFlipImage, GRMaskCreateRandom, GRStackImage
+NODE_CLASS_MAPPINGS = { "GR Prompt Selector" : GRPromptSelector , "GR Image Resize" : GRImageResize, "GR Mask Resize" : GRMaskResize, "GR Mask Create" : GRMaskCreate, "GR Multi Mask Create" : GRMultiMaskCreate, "GR Image Size": GRImageSize, "GR Tile and Border Image": GRTileImage, "GR Prompt Selector Multi": GRPromptSelectorMulti, "GR Tile and Border Image Random Flip" : GRTileFlipImage, "GR Mask Create Random": GRMaskCreateRandom, "GR Stack Image": GRStackImage }
 NODE_DISPLAY_NAME_MAPPINGS = { }
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
 
--- nodes.py
+++ nodes.py
@@ -1,3 +1,4 @@
+import random
 import os
 import math
 import numpy as np
@@ -5,13 +6,52 @@ import torch
 import torchvision.transforms.functional as TF
 import matplotlib.pyplot as plt
 import latent_preview
+import httplib2
 from clip import tokenize, model
 from PIL import Image, ImageDraw
 
 class GRPromptSelector:
     def __init__(self):
         pass
-        
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        clip_type = ("CLIP",)
+        string_type = ("STRING", {"multiline": True, "dynamicPrompts": True})
+        return {"required": {
+            "clip": clip_type,
+            **{f"positive_a{i}": string_type for i in range(1, 7)},
+            "always_a1": string_type,
+            "negative_a1": string_type,
+            "select_prompt": ("INT", {"default": 1, "min": 1, "max": 6}),
+        }}
+
+    RETURN_TYPES = ("CONDITIONING", "CONDITIONING", "STRING")
+    RETURN_NAMES = ("positive", "negative", "prompts")
+    FUNCTION = "select_prompt"
+    CATEGORY = "GraftingRayman"
+
+    def select_prompt(self, clip, **kwargs):
+        select_prompt = kwargs["select_prompt"]
+        positive_clip = kwargs[f"positive_a{select_prompt}"]
+        always_a1 = kwargs["always_a1"]
+        negative_a1 = kwargs["negative_a1"]
+
+        positive = f"{positive_clip}, {always_a1}"
+        prompts = f"positive:\n{positive}\n\nnegative:\n{negative_a1}"
+
+        tokensP = clip.tokenize(positive)
+        tokensN = clip.tokenize(negative_a1)
+        condP, pooledP = clip.encode_from_tokens(tokensP, return_pooled=True)
+        condN, pooledN = clip.encode_from_tokens(tokensN, return_pooled=True)
+
+        return ([[condP, {"pooled_output": pooledP}]], [[condN, {"pooled_output": pooledN}]], prompts)
+
+
+class GRPromptSelectorMulti:
+    def __init__(self):
+        pass
+
     @classmethod
     def INPUT_TYPES(cls):
         return {"required": {
@@ -22,40 +62,42 @@ class GRPromptSelector:
             "positive_a4": ("STRING", {"multiline": True, "dynamicPrompts": True}), "clip": ("CLIP", ), 
             "positive_a5": ("STRING", {"multiline": True, "dynamicPrompts": True}), "clip": ("CLIP", ), 
             "positive_a6": ("STRING", {"multiline": True, "dynamicPrompts": True}), "clip": ("CLIP", ), 
-            "always_a1": ("STRING", {"multiline": True, "dynamicPrompts": True}), "clip": ("CLIP", ), 
+            "alwayspositive_a1": ("STRING", {"multiline": True, "dynamicPrompts": True}), "clip": ("CLIP", ), 
             "negative_a1": ("STRING", {"multiline": True, "dynamicPrompts": True}), "clip": ("CLIP", ), 
-            "select_prompt": ("INT", {"default": 1, "min": 1, "max": 6}),
-        }}
+            }}
 
-    RETURN_TYPES = ("CONDITIONING","CONDITIONING","STRING",)
-    RETURN_NAMES = ("positive","negative","prompts",)
-    FUNCTION = "select_prompt"
+
+    RETURN_TYPES = ("CONDITIONING","CONDITIONING","CONDITIONING","CONDITIONING","CONDITIONING","CONDITIONING","CONDITIONING",)
+    RETURN_NAMES = ("positive1","positive2","positive3","positive4","positive5","positive6","negative",)
+    FUNCTION = "select_promptmulti"
     CATEGORY = "GraftingRayman"
         
 
 
-    def select_prompt(self, clip, positive_a1, positive_a2, positive_a3, positive_a4, positive_a5, positive_a6, always_a1, negative_a1, select_prompt):
+    def select_promptmulti(self, clip, positive_a1, positive_a2, positive_a3, positive_a4, positive_a5, positive_a6, alwayspositive_a1, negative_a1):
 
-        if select_prompt == 1:
-            clipa = positive_a1
-        elif select_prompt == 2:
-            clipa = positive_a2
-        elif select_prompt == 3:
-            clipa = positive_a3
-        elif select_prompt == 4:
-            clipa = positive_a4
-        elif select_prompt == 5:
-            clipa = positive_a5
-        elif select_prompt == 6:
-            clipa = positive_a6
-        positive = clipa + ", " + always_a1
-        prompts = "positive:\n" + positive + "\n\nnegative: \n" + negative_a1
-        tokensP = clip.tokenize(positive)
-        tokensN = clip.tokenize(negative_a1)
-        condP, pooledP = clip.encode_from_tokens(tokensP, return_pooled=True)
-        condN, pooledN = clip.encode_from_tokens(tokensN, return_pooled=True)
-        return ([[condP, {"pooled_output": pooledP}]],[[condN, {"pooled_output": pooledP}]], prompts )
+        positive1 = positive_a1 + ", " + alwayspositive_a1
+        positive2 = positive_a2 + ", " + alwayspositive_a1
+        positive3 = positive_a3 + ", " + alwayspositive_a1
+        positive4 = positive_a4 + ", " + alwayspositive_a1
+        positive5 = positive_a5 + ", " + alwayspositive_a1
+        positive6 = positive_a6 + ", " + alwayspositive_a1
+        tokensP1 = clip.tokenize(positive1)
+        tokensP2 = clip.tokenize(positive2)
+        tokensP3 = clip.tokenize(positive3)
+        tokensP4 = clip.tokenize(positive4)
+        tokensP5 = clip.tokenize(positive5)
+        tokensP6 = clip.tokenize(positive6)
+        tokensN1 = clip.tokenize(negative_a1)
+        condP1, pooledP1 = clip.encode_from_tokens(tokensP1, return_pooled=True)
+        condP2, pooledP2 = clip.encode_from_tokens(tokensP2, return_pooled=True)
+        condP3, pooledP3 = clip.encode_from_tokens(tokensP3, return_pooled=True)
+        condP4, pooledP4 = clip.encode_from_tokens(tokensP4, return_pooled=True)
+        condP5, pooledP5 = clip.encode_from_tokens(tokensP5, return_pooled=True)
+        condP6, pooledP6 = clip.encode_from_tokens(tokensP6, return_pooled=True)
+        condN1, pooledN1 = clip.encode_from_tokens(tokensN1, return_pooled=True)
 
+        return ([[condP1, {"pooled_output": pooledP1}]],[[condP2, {"pooled_output": pooledP2}]],[[condP3, {"pooled_output": pooledP3}]],[[condP4, {"pooled_output": pooledP4}]],[[condP5, {"pooled_output": pooledP5}]],[[condP6, {"pooled_output": pooledP6}]],[[condN1, {"pooled_output": pooledN1}]],)
 
 
 class GRImageResize:
@@ -305,5 +347,193 @@ class GRTileImage:
             "purple": [128, 0, 128],
             "yellow": [255, 255, 0],
 
+        }
+        return torch.tensor(color_map[colour], dtype=torch.float32)
+
+class GRTileFlipImage:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        popular_colors = [
+            "black", "white", "red", "blue", "green", "purple", "yellow"
+        ]
+        return {
+            "required": {
+                "image": ("IMAGE",),
+                "columns": ("INT", {"min": 1}),
+                "rows": ("INT", {"min": 1}),
+                "border": ("INT", {"min": 0, "default": 0}),
+                "colour": (popular_colors,),
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "tile_image"
+    CATEGORY = "GraftingRayman"
+
+    def tile_image(self, image, rows, columns, colour, border=0):
+        batch_size, orig_height, orig_width, channels = image.size()
+
+        if border > 0:
+            orig_height_with_border = orig_height + 2 * border
+            orig_width_with_border = orig_width + 2 * border
+
+            bordered_image = torch.ones((batch_size, orig_height_with_border, orig_width_with_border, channels), dtype=image.dtype, device=image.device)
+            bordered_image[:, border:-border, border:-border, :] = image
+            border_color = self.get_colour_value(colour)
+            bordered_image[:, :border, :, :] = border_color  # Top border
+            bordered_image[:, -border:, :, :] = border_color  # Bottom border
+            bordered_image[:, :, :border, :] = border_color  # Left border
+            bordered_image[:, :, -border:, :] = border_color  # Right border
+        else:
+            bordered_image = image
+
+        new_height = rows * orig_height_with_border if border > 0 else rows * orig_height
+        new_width = columns * orig_width_with_border if border > 0 else columns * orig_width
+        new_image = torch.zeros((batch_size, new_height, new_width, channels), dtype=image.dtype, device=image.device)
+
+        num_tiles_height = (new_height + orig_height_with_border - 1) // orig_height_with_border
+        num_tiles_width = (new_width + orig_width_with_border - 1) // orig_width_with_border
+
+        flip_tile = (random.randint(0, rows - 1), random.randint(0, columns - 1))
+
+        for i in range(rows):
+            for j in range(columns):
+                y_start = i * orig_height_with_border
+                y_end = min(y_start + orig_height_with_border, new_height)
+                x_start = j * orig_width_with_border
+                x_end = min(x_start + orig_width_with_border, new_width)
+
+                tile = bordered_image[:, :y_end - y_start, :x_end - x_start, :]
+
+                if (i, j) == flip_tile:
+                    tile = torch.flip(tile, dims=[2])
+
+                new_image[:, y_start:y_end, x_start:x_end, :] = tile
+
+        return (new_image,)
+
+    def get_colour_value(self, colour):
+        # Map color names to RGB values
+        color_map = {
+            "black": [0, 0, 0],
+            "white": [255, 255, 255],
+            "red": [255, 0, 0],
+            "blue": [0, 0, 255],
+            "green": [0, 255, 0],
+            "purple": [128, 0, 128],
+            "yellow": [255, 255, 0],
+        }
+        return torch.tensor(color_map[colour], dtype=torch.float32)
+
+class GRMaskCreateRandom:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "height": ("INT", {"min": 1}),
+                "width": ("INT", {"min": 1}),
+                "mask_size": ("FLOAT", {"min": 0, "max": 1}),
+                "seed": ("INT", {"min": 1, "default": 0}),
+            },
+        }
+
+    RETURN_TYPES = ("MASK",)
+    FUNCTION = "create_mask"
+    CATEGORY = "GraftingRayman"
+
+    def create_mask(self, height, width, mask_size, seed):
+        mask_dim = int(min(height, width) * mask_size)
+        
+        if mask_dim == 0:
+            raise ValueError("mask_size is too small, resulting in zero mask dimensions.")
+        
+        mask = torch.zeros((1, 1, height, width), dtype=torch.float32)
+        
+        x_start = random.randint(0, width - mask_dim)
+        y_start = random.randint(0, height - mask_dim)
+        
+        mask[:, :, y_start:y_start + mask_dim, x_start:x_start + mask_dim] = 1.
+        
+        return mask
+
+class GRStackImage:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        popular_colors = [
+            "black", "white", "red", "blue", "green", "purple", "yellow"
+        ]
+        return {
+            "required": {
+                "image1": ("IMAGE",),
+                "image2": ("IMAGE",),
+                "border": ("INT", {"min": 0, "default": 0}),
+                "colour": (popular_colors,),
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "stack_images"
+    CATEGORY = "GraftingRayman"
+
+    def stack_images(self, image1, image2, colour, border=0):
+        batch_size1, orig_height1, orig_width1, channels1 = image1.size()
+        batch_size2, orig_height2, orig_width2, channels2 = image2.size()
+
+        if batch_size1 != batch_size2 or channels1 != channels2 or orig_width1 != orig_width2:
+            raise ValueError("Images must have the same batch size, width, and number of channels.")
+        
+        if border > 0:
+            orig_height1_with_border = orig_height1 + 2 * border
+            orig_height2_with_border = orig_height2 + 2 * border
+            orig_width_with_border = orig_width1 + 2 * border
+            
+            bordered_image1 = torch.ones((batch_size1, orig_height1_with_border, orig_width_with_border, channels1), dtype=image1.dtype, device=image1.device)
+            bordered_image2 = torch.ones((batch_size2, orig_height2_with_border, orig_width_with_border, channels2), dtype=image2.dtype, device=image2.device)
+
+            bordered_image1[:, border:-border, border:-border, :] = image1
+            bordered_image2[:, border:-border, border:-border, :] = image2
+
+            border_color = self.get_colour_value(colour)
+            bordered_image1[:, :border, :, :] = border_color  # Top border
+            bordered_image1[:, -border:, :, :] = border_color  # Bottom border
+            bordered_image1[:, :, :border, :] = border_color  # Left border
+            bordered_image1[:, :, -border:, :] = border_color  # Right border
+
+            bordered_image2[:, :border, :, :] = border_color  # Top border
+            bordered_image2[:, -border:, :, :] = border_color  # Bottom border
+            bordered_image2[:, :, :border, :] = border_color  # Left border
+            bordered_image2[:, :, -border:, :] = border_color  # Right border
+        else:
+            bordered_image1 = image1
+            bordered_image2 = image2
+
+        new_height = (orig_height1_with_border if border > 0 else orig_height1) + (orig_height2_with_border if border > 0 else orig_height2)
+        new_width = orig_width_with_border if border > 0 else orig_width1
+        new_image = torch.zeros((batch_size1, new_height, new_width, channels1), dtype=image1.dtype, device=image1.device)
+
+        new_image[:, :bordered_image1.size(1), :, :] = bordered_image1
+        new_image[:, bordered_image1.size(1):, :, :] = bordered_image2
+
+        return (new_image,)
+
+    def get_colour_value(self, colour):
+        # Map color names to RGB values
+        color_map = {
+            "black": [0, 0, 0],
+            "white": [255, 255, 255],
+            "red": [255, 0, 0],
+            "blue": [0, 0, 255],
+            "green": [0, 255, 0],
+            "purple": [128, 0, 128],
+            "yellow": [255, 255, 0],
         }
         return torch.tensor(color_map[colour], dtype=torch.float32)
