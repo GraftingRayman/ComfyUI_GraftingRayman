d6bbae1962612c3446e6a44d05ca52057b0c741b
GraftingRayman <156515434+GraftingRayman@users.noreply.github.com>
2024-06-16 08:51:50 +0100

    separating nodes

--- GRnodes.py
+++ GRnodes.py
@@ -446,39 +446,6 @@ class GRTileFlipImage:
         }
         return torch.tensor(color_map[colour], dtype=torch.float32)
 
-class GRMaskCreateRandom:
-    def __init__(self):
-        pass
-
-    @classmethod
-    def INPUT_TYPES(cls):
-        return {
-            "required": {
-                "height": ("INT", {"min": 1}),
-                "width": ("INT", {"min": 1}),
-                "mask_size": ("FLOAT", {"min": 0, "max": 1}),
-                "seed": ("INT", {"min": 1, "default": 0}),
-            },
-        }
-
-    RETURN_TYPES = ("MASK",)
-    FUNCTION = "create_mask"
-    CATEGORY = "GraftingRayman"
-
-    def create_mask(self, height, width, mask_size, seed):
-        mask_dim = int(min(height, width) * mask_size)
-        
-        if mask_dim == 0:
-            raise ValueError("mask_size is too small, resulting in zero mask dimensions.")
-        
-        mask = torch.zeros((1, 1, height, width), dtype=torch.float32)
-        
-        x_start = random.randint(0, width - mask_dim)
-        y_start = random.randint(0, height - mask_dim)
-        
-        mask[:, :, y_start:y_start + mask_dim, x_start:x_start + mask_dim] = 1.
-        
-        return mask
 
 class GRStackImage:
     def __init__(self):
--- __init__.py
+++ __init__.py
@@ -1,5 +1,9 @@
-from .GRnodes import GRPromptSelector, GRImageResize, GRMaskResize, GRMaskCreate, GRMultiMaskCreate, GRImageSize, GRTileImage, GRPromptSelectorMulti, GRTileFlipImage, GRMaskCreateRandom, GRStackImage, GRResizeImageMethods, GRImageDetailsDisplayer, GRImageDetailsSave, GRFlipTileRedRing, GRFlipTileInverted
+from .GRnodes import GRPromptSelector, GRImageResize, GRMaskResize, GRMaskCreate, GRMultiMaskCreate,  GRTileImage, GRPromptSelectorMulti, GRTileFlipImage,  GRStackImage, GRResizeImageMethods, GRImageDetailsDisplayer, GRImageDetailsSave, GRFlipTileRedRing, GRFlipTileInverted
+from .GRMaskCreateRandom import GRMaskCreateRandom
+from .GRImageSize import GRImageSize
+
 NODE_CLASS_MAPPINGS = { "GR Prompt Selector" : GRPromptSelector , "GR Image Resize" : GRImageResize, "GR Mask Resize" : GRMaskResize, "GR Mask Create" : GRMaskCreate, "GR Multi Mask Create" : GRMultiMaskCreate, "GR Image Size": GRImageSize, "GR Tile and Border Image": GRTileImage, "GR Prompt Selector Multi": GRPromptSelectorMulti, "GR Tile and Border Image Random Flip" : GRTileFlipImage, "GR Mask Create Random": GRMaskCreateRandom, "GR Stack Image": GRStackImage, "GR Image Resize Methods" : GRResizeImageMethods,"GR Image Details Displayer": GRImageDetailsDisplayer, "GR Image Details Saver": GRImageDetailsSave, "GR Flip Tile Random Red Ring": GRFlipTileRedRing, "GR Flip Tile Random Inverted": GRFlipTileInverted }
+
 NODE_DISPLAY_NAME_MAPPINGS = { }
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
 
